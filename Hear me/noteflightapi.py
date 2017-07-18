ddf21fa2bdbc820eeb004360db5ed44402c82d4e
nicolediaz <nicolediaz1525@gmail.com>
2017-07-18 11:04:57 -0500

    Everything Has Changed

--- Hear me/noteflightapi.py
+++
@@ -1,5 +0,0 @@
- <script src="http://www.noteflight.com/clientapi/2.0.0/nfclient.js"></script>
-  // Initialize the Noteflight client API.
-  NFClient.init(function(info) {
-    alert("Noteflight API is ready, version " + info.version);
-  });
